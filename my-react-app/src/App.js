c9fde2834d4e420dde51e8b8563dcfaf15b5d70c
HrishikeshJoshi01 <hrishikeshjoshi2801@gmail.com>
2021-12-02 22:27:34 +0530

    Started React App

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div><h1>Hello, World!</h1>
+      <h2>This is my First React App</h2>
+    </div>
+  );
+}
+
+export default App;
